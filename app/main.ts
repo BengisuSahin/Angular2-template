f114ee13ce4f3208989b02cd67b05892072b32e5
BengisuSahin <sahinbengisu@gmail.com>
2016-06-03 14:03:48 +0300

    Add files via upload

---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './app.component'; // (./) That means the source module is in the same folder
+
+bootstrap(AppComponent);
